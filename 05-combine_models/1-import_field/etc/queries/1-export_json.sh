ab49af1bddc60647add316a4f2069c7b408b1f4e
Rafa Gómez <rgomezcasas@gmail.com>
2024-07-16 16:47:43 +0200

    feat(5-1): add base
    Former-commit-id: e6f093853113f4691adf4e35bfd77423cb507892

--- 05-combine_models/1-import_field/etc/queries/1-export_json.sh
+++
@@ -1,1 +0,0 @@
-PGPASSWORD=c0d3ly7v psql -h localhost -p 5432 -U codely -d ecommerce -c "\COPY (SELECT row_to_json(u) FROM shop.users u) TO 'postgres_dump-shop.users.json';"
--- 05-combine_models/1-import_field/etc/queries/2-import_to_elasticsearch.sh
+++
@@ -1,1 +0,0 @@
-docker-compose exec -T 4_1_first_importation-logstash logstash -f /usr/share/logstash/pipeline/import-json.conf
