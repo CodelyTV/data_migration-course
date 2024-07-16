3edbb01b26ab167d7ff8930561b35cae182d7020
Rafa Gómez <rgomezcasas@gmail.com>
2024-07-16 16:21:19 +0200

    feat(4-1): add example
    Former-commit-id: 06bb6ed1f7478a8bdb7997c88e3ed943a0a8ced2

--- 04-no_sql/1-csv_to_elastic/etc/queries/2-export_postgres_import_mysql/1-create_postgres_view.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW shop.retention__users AS
-SELECT
-	id,
-	email,
-	created_at AS last_activity_date,
-	created_at AS registration_date
-FROM
-	shop.users;
--- 04-no_sql/1-csv_to_elastic/etc/queries/2-export_postgres_import_mysql/3-dump_postgres_table.sh
+++
@@ -1,1 +0,0 @@
-PGPASSWORD=c0d3ly7v pg_dump -h localhost -p 5432 -U codely -t "shop.users" ecommerce -F p --file=postgres_dump-shop.users.sql
