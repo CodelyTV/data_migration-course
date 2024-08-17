dc86e434466b9a5b830e6c34b833c547fb268e7d
Javier Ferrer González <javier.ferrer@codely.com>
2024-08-17 19:38:44 +0200

    fix: replace docker-compose by docker compose due to GitHub removing support

--- 04-no_sql/1-csv_to_elastic/etc/queries/2-import_to_elasticsearch.sh
+++ 04-no_sql/1-csv_to_elastic/etc/queries/2-import_to_elasticsearch.sh
@@ -1,1 +1,1 @@
-docker-compose exec -T 4_1_first_importation-logstash logstash -f /usr/share/logstash/pipeline/import-csv.conf
+docker compose exec -T 4_1_first_importation-logstash logstash -f /usr/share/logstash/pipeline/import-csv.conf
